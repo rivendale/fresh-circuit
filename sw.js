e38b9d536fc602657269ca0372ea0addecd05791
Nick Rygiel <nick.ryg@gmail.com>
2026-09-19 17:17:43 -0400

    Bump cache to v3 after core.js parse fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fresh-circuit-v2";
+const CACHE = "fresh-circuit-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,37 +9,36 @@ const ASSETS = [
   "./icons/icon.svg"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      Promise.all(ASSETS.map((url) => cache.add(url).catch(() => null)))
-    ).then(() => self.skipWaiting())
+    caches.open(CACHE).then(function (cache) {
+      return Promise.all(ASSETS.map(function (url) {
+        return cache.add(url).catch(function () { return null; });
+      }));
+    }).then(function () { return self.skipWaiting(); })
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function (event) {
   const req = event.request;
   if (req.method !== "GET") return;
   event.respondWith(
-    caches.match(req).then((cached) => {
-      const fetchPromise = fetch(req)
-        .then((res) => {
-          if (res && res.status === 200 && req.url.startsWith(self.location.origin)) {
-            const copy = res.clone();
-            caches.open(CACHE).then((cache) => cache.put(req, copy));
-          }
-          return res;
-        })
-        .catch(() => cached);
-      return cached || fetchPromise;
+    fetch(req).then(function (res) {
+      if (res && res.status === 200 && req.url.indexOf(self.location.origin) === 0) {
+        const copy = res.clone();
+        caches.open(CACHE).then(function (cache) { cache.put(req, copy); });
+      }
+      return res;
+    }).catch(function () {
+      return caches.match(req);
     })
   );
 });
